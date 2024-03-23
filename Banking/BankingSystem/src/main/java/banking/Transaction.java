9664ca78fd981b9e25765ea9922c79644fefc502
LAPTOP-8OT3SL65 <ayush.ece.1713@iiitbh.ac.in>
2024-03-24 01:13:49 +0530

    java

---
+++ Banking/BankingSystem/src/main/java/banking/Transaction.java
@@ -0,0 +1,49 @@
+package banking;
+
+/**
+ *
+ * Private Variables:<br>
+ * {@link #accountNumber}: Long<br>
+ * {@link #bank}: Bank<br>
+ */
+public class Transaction extends Bank implements TransactionInterface{
+	//private final int attemptedPin;
+	private Long accountNumber;
+	private Bank bank;
+
+	/**
+	 *
+	 * @param bank
+	 *            The bank where the account is housed.
+	 * @param accountNumber
+	 *            The customer's account number.
+	 * @param attemptedPin
+	 *            The PIN entered by the customer.
+	 * @throws Exception
+	 *             Account validation failed.
+	 */
+	public Transaction(Bank bank, Long accountNumber, int attemptedPin) throws Exception {
+		// complete the function
+		this.bank=bank;
+		this.accountNumber=accountNumber;
+		if(!bank.authenticateUser(accountNumber,attemptedPin)){
+			throw new Exception("Account validation failed.");
+		}
+	}
+
+	public double getBalance() {
+		 return bank.getBalance(accountNumber);
+       // return -1;
+	}
+
+	public void credit(double amount) {
+		// complete the function
+		bank.credit(accountNumber,amount);
+		
+	}
+
+	public boolean debit(double amount) {
+		// complete the function
+		return bank.debit(accountNumber,amount);
+	}
+}
---
+++ src/main/java/banking/Bank.java
@@ -0,0 +1,69 @@
+package banking;
+
+import java.util.LinkedHashMap;
+
+/**
+ * Private Variables:<br>
+ * {@link #accounts}: List&lt;Long, Account&gt;
+ */
+public class Bank{
+	private LinkedHashMap<Long, Account> accounts;
+	static long i=1000000;
+
+
+
+	public Bank() {
+		accounts=new LinkedHashMap<>();
+	}
+
+	private Long getAccount(Long accountNumber) {
+
+		return accountNumber;
+	}
+
+	public Long openCommercialAccount(Company company, int pin, double startingDeposit) {
+		Long accountNumber=generateUniqueAccountNumber();
+		CommercialAccount commercialAccount = new CommercialAccount(company,accountNumber,pin,startingDeposit);
+		accounts.put(accountNumber,commercialAccount);
+		return  accountNumber;
+	}
+
+	public Long openConsumerAccount(Person person, int pin, double startingDeposit) {
+		Long accountNumber=generateUniqueAccountNumber();
+		ConsumerAccount consumerAccount=new ConsumerAccount(person,accountNumber,pin,startingDeposit);
+		accounts.put(accountNumber,consumerAccount);
+        return accountNumber;
+	}
+
+	private Long generateUniqueAccountNumber() {
+		return i++;
+	}
+
+	public boolean authenticateUser(Long accountNumber, int pin) {
+
+		Account account = accounts.get(accountNumber);
+        return account.validatePin(pin) && account!=null && account.getAccountNumber().equals(accountNumber);
+	}
+
+	public double getBalance(Long accountNumber) {
+
+		Account account = accounts.get(accountNumber);
+		return account.getBalance();
+
+	}
+
+	public void credit(Long accountNumber, double amount) {
+
+		Account account = accounts.get(accountNumber);
+		account.creditAccount(amount);
+	}
+
+	public boolean debit(Long accountNumber, double amount) {
+
+		Account account = accounts.get(accountNumber);
+		if(account.getBalance()>0 && account.getBalance()>=amount) {
+			return account.debitAccount(amount);
+		}
+        return false;
+	}
+}
